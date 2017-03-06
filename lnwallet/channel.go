a011122220233eccce128e82ae79447eeb896a01
Andrey Samokhvalov <andrew.shvv@gmail.com>
2017-03-06 17:00:32 +0300

    lnwallet: add missed addition of RHash
    add rhash to the payment descriptor when receiving the settle htlc in
    order to be able to pass it during settle htlc packet generation and
    later find the user pending payment by rhash without additional hashing.

--- lnwallet/channel.go
+++ lnwallet/channel.go
@@ -2107,6 +2107,7 @@ func (lc *LightningChannel) ReceiveHTLCSettle(preimage [32]byte, logIndex uint64
 		Amount:      htlc.Amount,
 		RPreimage:   preimage,
 		ParentIndex: htlc.Index,
+		RHash:       htlc.RHash,
 		Index:       lc.remoteUpdateLog.logIndex,
 		EntryType:   Settle,
 	}
