cc89d071c9096d75082e5c4eec0d91246bba4d2a
Andrey Samokhvalov <andrew.shvv@gmail.com>
2017-05-02 23:04:58 +0300

    htlcswicth: start use htlcswitch and channel link inside lnd
    In current commit big shift have been made in direction of unit testable
    payments scenarios. Previosly two additional structures have been added
    which had been spreaded in the lnd package before, and now we apply
    them in the lnd itself:
    
    1. ChannelLink - is an interface which represents the subsystem for
    managing the incoming htlc requests, applying the changes to the
    channel, and also propagating/forwarding it to htlc switch.
    
    2. Switch - is a central messaging bus for all incoming/outgoing htlc's.
    The goal of the switch is forward the incoming/outgoing htlc messages
    from one channel to another, and also propagate the settle/fail htlc
    messages back to original requester.
    
    With this abtractions the folowing schema becomes nearly complete:
    
    abstraction
        ^
        |
        | - - - - - - - - - - - - Lightning - - - - - - - - - - - - -
        |
        | (Switch)		        (Switch)		  (Switch)
        |  Alice <-- channel link --> Bob <-- channel link --> Carol
        |
        | - - - - - - - - - - - - - TCP - - - - - - - - - - - - - - -
        |
        |  (Peer) 		        (Peer)	                  (Peer)
        |  Alice <----- tcp conn --> Bob <---- tcp conn -----> Carol

--- rpcserver.go
+++ rpcserver.go
@@ -19,6 +19,7 @@ import (
 	"github.com/boltdb/bolt"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/lightningnetwork/lnd/channeldb"
+	"github.com/lightningnetwork/lnd/htlcswitch"
 	"github.com/lightningnetwork/lnd/lnrpc"
 	"github.com/lightningnetwork/lnd/lnwallet"
 	"github.com/lightningnetwork/lnd/lnwire"
@@ -526,7 +527,7 @@ func (r *rpcServer) OpenChannelSync(ctx context.Context,
 	}
 }
 
-// CloseChannel attempts to close an active channel identified by its channel
+// CloseLink attempts to close an active channel identified by its channel
 // point. The actions of this method can additionally be augmented to attempt
 // a force close after a timeout period in the case of an inactive peer.
 func (r *rpcServer) CloseChannel(in *lnrpc.CloseChannelRequest,
@@ -624,7 +625,7 @@ func (r *rpcServer) CloseChannel(in *lnrpc.CloseChannelRequest,
 		// the htlc switch which will handle the negotiation and
 		// broadcast details.
 		updateChan, errChan = r.server.htlcSwitch.CloseLink(chanPoint,
-			CloseRegular)
+			htlcswitch.CloseRegular)
 	}
 out:
 	for {
